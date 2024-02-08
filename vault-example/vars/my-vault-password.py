2924fc48261c2687fdcc46991e05880fbc0b3f9d
Mona Lisa <surya.vungarala@jpmchase.com>
2024-02-08 07:19:17 +0000

    learning about containers and ansible cicd modules

---
+++ vault-example/vars/my-vault-password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser@alta3.com
+   A simple script that returns a vault password"""
+
+def main():
+    """"returns the decrypt password"""
+    print("Mayuk@20")
+
+if __name__ == "__main__":
+    main()
+
